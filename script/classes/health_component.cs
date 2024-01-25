420d7f35060affb777d82a9e40d5368183abb915
GulpeIT <grishawork-01@mail.ru>
2024-01-25 14:27:09 +0300

    Add damage package

--- script/classes/health_component.cs
+++ script/classes/health_component.cs
@@ -4,5 +4,13 @@ using System;
 public partial class healthComponent : Node2D
 {
 	[Export] public float health;
-	[Export] public float armory;
+	[Export] public float defense;
+	
+	public void TakeDamage(damagePackage damage){
+		health -= Mathf.Max(damage.GetDamage() - Mathf.Max(defense - damage.GetPenetration(), 0), 0);
+	}
+
+	public float GetHealth(){
+		return health;
+	}
 }
